19dfb398e9ed67b02d4cbc9c2dcbd5314b3e1ccf
AntorFR <contact@antor.fr>
2021-01-18 23:55:41 +0100

    led.on

--- src/SmartIoT/Boot/BootStandaloneConst.hpp
+++ src/SmartIoT/Boot/BootStandaloneConst.hpp
@@ -5,12 +5,12 @@
 namespace SmartIotInternals {
 
 
-  const char WIFI_SSID[] = "SSID";
-  const char WIFI_PSWD[] = "pswd";
+  const char WIFI_SSID[] = "INTRA_M2M";
+  const char WIFI_PSWD[] = "u-#7_2@!qMmkgScVRGb=PcZW$tAQewj7";
 
   const char MQTT_SERVER[] = "192.168.10.23";
   const bool MQTT_AUTH = true;
-  const char MQTT_USER[] = "user";
-  const char MQTT_PSWD[] = "pswd";
+  const char MQTT_USER[] = "antor";
+  const char MQTT_PSWD[] = "s2berard";
 
 }
