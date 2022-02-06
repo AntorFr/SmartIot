9fd532d15048e3e4efad7e2719211711dd52cdfd
AntorFR <contact@antor.fr>
2022-02-06 10:05:37 +0100

    Add:rainbow clock pattern

--- src/Nodes/LedMatrix/LedMatrixPattern.cpp
+++ src/Nodes/LedMatrix/LedMatrixPattern.cpp
@@ -13,22 +13,24 @@ void ClockPattern::init(){
     _cTxt.SetFont(MatriseFontData);
     _cTxt.Init(_matrix, _matrix->Width(), _cTxt.FontHeight() + 1, 0, 0);
 
-    strcpy((char *)(_txt), Interface::get().getTime().getShortTime());
-    minutes = Interface::get().getTime().getTm()->tm_min;
-
+    fill_solid(_leds,_nbLed, CRGB::Black); 
+    minutes = 100;
+    show();
 }
 
 void ClockPattern::display(){
-
     uint8_t min = Interface::get().getTime().getTm()->tm_min;
-    if (minutes!=min){
-        strcpy((char *)(_txt), Interface::get().getTime().getShortTime());
-        minutes=min;
-    }
 
     uint8_t hue = beat8(_obj->getSpeed()/3);
     uint8_t delta = 2*_matrix->Width();
 
+    if (minutes!=min || minutes==100){
+        strcpy((char *)(_txt), Interface::get().getTime().getShortTime());
+        minutes=min;
+
+        
+        
+    }
     _cTxt.SetText(_txt, sizeof(_txt) - 1);
     _cTxt.SetTextColrOptions(COLR_HSV | COLR_GRAD_AH,hue,0xff,0xff,0xff,0xff,(hue+delta)%255);
     _cTxt.UpdateText();
